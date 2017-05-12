d57b4f95755d97ff881ba28fe4e808b10266d5ee
wangyingjia <1795505822@qq.com>
2017-05-12 15:31:26 +0800

    src

---
+++ src/com/mfw/util/ServiceHelper.java
@@ -0,0 +1,28 @@
+package com.mfw.util;
+
+import com.mfw.service.system.menu.MenuService;
+import com.mfw.service.system.role.RoleService;
+import com.mfw.service.system.user.UserService;
+
+/**
+ * @author Administrator 获取Spring容器中的service bean
+ */
+public final class ServiceHelper {
+
+	public static Object getService(String serviceName) {
+		//WebApplicationContextUtils.
+		return Const.WEB_APP_CONTEXT.getBean(serviceName);
+	}
+
+	public static UserService getUserService() {
+		return (UserService) getService("userService");
+	}
+
+	public static RoleService getRoleService() {
+		return (RoleService) getService("roleService");
+	}
+
+	public static MenuService getMenuService() {
+		return (MenuService) getService("menuService");
+	}
+}
